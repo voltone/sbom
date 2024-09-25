f72e440e144af6f91d387e274b5603ff7842c593
Tomasz Kowal <tomekowal@gmail.com>
2024-09-25 07:36:43 +0000

    Fix tests when running new converger

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule SBoM.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:hex, :xmerl]
+      extra_applications: [:logger, :hex, :xmerl]
     ]
   end
 
